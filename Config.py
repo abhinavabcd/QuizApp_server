94eb8e540ace9c4eb7bf56aac9c138c8430db336
abhinav2 <asolipuram@kiwiup.com>
2014-09-10 12:24:54 +0530

    external server map vs internal server map

--- Config.py
+++ Config.py
@@ -8,8 +8,14 @@ import datetime
 
 SERVER_ID = "1"
 WebServersMap = {
-                 "master":"http://192.168.0.10:8084"
+                 "master":"http://192.168.0.10:8085"
                 # "1":"http://192.168.0.10:8083"
                 }
+
+ExternalWebServersMap = {
+                 "master":"http://192.168.0.10:8085"
+                # "1":"http://192.168.0.10:8083"
+                }
+
 
 dbServer = ["192.168.0.10",27017,datetime.date(2014, 8 , 27) , 10] #10 => priority to choost/computing power
