b016fb589a2a42ffb178ef817971f529c8644830
abhinav2 <asolipuram@kiwiup.com>
2014-09-16 21:03:27 +0530

    modifications and new servercalls to get leader boards

--- Constants.py
+++ Constants.py
@@ -45,6 +45,7 @@ OK_ACTIVATING_BOT = 511
 NO_REPLY_FROM_OTHER_USERS=512
 OK_SEND_MESSAGE = 513
 OK_USERS_INFO = 514
+OK_SCORE_BOARD = 515
 ################################# dict values/commands for payload type definition
 USER_ANSWERED_QUESTION = 1
 GET_NEXT_QUESTION = 2
