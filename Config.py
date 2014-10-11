63d4f32fd1d182b7b7f72adf5516412b790d3c28
Vinay <vinay.bhargav.reddy@gmail.com>
2014-10-11 19:12:04 +0530

    minor changes to change ip easily

--- Config.py
+++ Config.py
@@ -7,15 +7,17 @@ Created on Aug 26, 2014
 import datetime
 
 SERVER_ID = "master"
+IP = "192.168.0.10"
+
 WebServersMap = {
-                 "master":"http://192.168.0.10:8085"
+                 "master":"http://"+IP+":8085"
                 # "1":"http://192.168.0.10:8083"
                 }
 
 ExternalWebServersMap = {
-                 "master":"http://192.168.0.10:8085"
+                 "master":"http://"+IP+":8085"
                 # "1":"http://192.168.0.10:8083"
                 }
 
 
-dbServer = ["192.168.0.10",27017,datetime.date(2014, 8 , 27) , 10] #10 => priority to choost/computing power
+dbServer = [IP,27017,datetime.date(2014, 8 , 27) , 10] #10 => priority to choost/computing power
