88b2689864b45f269519da70b5af39d1db8a8ba0
abhinav2 <asolipuram@kiwiup.com>
2014-10-19 11:36:38 +0530

    print pid

--- server.py
+++ server.py
@@ -531,6 +531,7 @@ class QuizApp(tornado.web.Application):
 
 
 def main():
+    print "PROCESS_PID: "+os.getpid()
     http_server = tornado.httpserver.HTTPServer(QuizApp())
     http_server.listen(HTTP_PORT)
     tornado.ioloop.PeriodicCallback(sendGcmMessages, 2000).start()
